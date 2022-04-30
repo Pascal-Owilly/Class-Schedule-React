93daab89afa53a954aa39c49d72ac9721226261b
Owilly <pasaclouma54@gmail.com>
2022-04-30 22:52:23 +0300

    add registration route

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,53 +1,105 @@
-import React from "react";
-import profile from '../images/profile_pic.jpg';
-import email from '../images/email.jpg';
-import password from '../images/password.jpg';
-import './signup.css';
-function SignUp() {
-  return (
-    <div className="main">
-     <div className="sub-main">
-       <div>
-         <div className="imgs">
-           <div className="container-image">
-             <img src={profile} alt="profile" className="profile"/>
+import React from 'react'
+import { useState } from 'react';
+import './signup.css'
+export default function Form() {
 
-           </div>
+// States for registration
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [password, setPassword] = useState('');
 
+// States for checking the errors
+const [submitted, setSubmitted] = useState(false);
+const [error, setError] = useState(false);
 
-         </div>
-         <div>
-           <h1>Sign up</h1>
-           <div>
-             <img src={email} alt="email" className="email"/>
-             <input type="text" placeholder="user name" className="name"/>
-           </div>
-           <div className="second-input">
-             <img src={password} alt="password" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
-           </div>
+// Handling the name change
+const handleName = (e) => {
+setName(e.target.value);
+setSubmitted(false);
+};
 
-           <div className="second-input">
-             
-             <img src={password} alt="password" className="email"/>
-             <input type="password" placeholder="user name" className="name"/>
-           </div>
-          <div className="login-button">
-          <button>Sign Up</button>
-          </div>
-           
-            <p className="link">
-              {/* <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a> */}
-            </p>
-           
- 
-         </div>
-       </div>
-       
+// Handling the email change
+const handleEmail = (e) => {
+setEmail(e.target.value);
+setSubmitted(false);
+};
 
-     </div>
-    </div>
-  );
+// Handling the password change
+const handlePassword = (e) => {
+setPassword(e.target.value);
+setSubmitted(false);
+};
+
+// Handling the form submission
+const handleSubmit = (e) => {
+e.preventDefault();
+if (name === '' || email === '' || password === '') {
+setError(true);
+} else {
+setSubmitted(true);
+setError(false);
 }
+};
 
-export default SignUp;
+// Showing success message
+const successMessage = () => {
+return (
+<div
+className="success"
+style={{
+display: submitted ? '' : 'none',
+}}>
+<h3>Welcome aboard {name}!!</h3>
+</div>
+);
+};
+
+// Showing error message if error is true
+const errorMessage = () => {
+return (
+<div
+className="error"
+style={{
+display: error ? '' : 'none',
+}}>
+<h1>Please enter all the fields</h1>
+</div>
+);
+};
+
+return (
+<div className="form">
+<div>
+<h1>Register</h1>
+<hr />
+</div>
+
+{/* Calling to the methods */}
+<div className="messages">
+{errorMessage()}
+{successMessage()}
+</div>
+
+<form>
+{/* Labels and inputs for form data */}
+<label className="label">Full Name</label>
+<input onChange={handleName} className="input"
+value={name} type="text" placeholder='Enter your name here ...' />
+
+<label className="label">Email</label>
+<input onChange={handleEmail} className="input"
+value={email} type="email" placeholder='Enter your email ...' />
+
+<label className="label">Password</label>
+<input onChange={handlePassword} className="input"
+value={password} type="password" placeholder='Enter Password' />
+
+<button onClick={handleSubmit} className="btn" type="submit">
+Submit
+</button>
+</form>
+<hr /><br />
+ <p className='login-text>'>Already have an account? &nbsp; <span>Sign in</span></p>
+</div>
+);
+}
