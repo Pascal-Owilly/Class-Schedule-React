1ef4d7e93dbe2bf190e576cf1cee63b5900c6ac5
Mercy <mercycherotich757@gmail.com>
2022-05-13 00:01:19 +0300

    deploy

---
+++ build/precache-manifest.812ae6c511f370ccba6d201e802dcf77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753500c577afbedba23cf427e67162b9",
+    "url": "/Class-Schedule-React/index.html"
+  },
+  {
+    "revision": "7928c7381be47ef41f23",
+    "url": "/Class-Schedule-React/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "9bf957aa6cbcaa876dc2",
+    "url": "/Class-Schedule-React/static/css/main.741a164a.chunk.css"
+  },
+  {
+    "revision": "7928c7381be47ef41f23",
+    "url": "/Class-Schedule-React/static/js/2.7f73729d.chunk.js"
+  },
+  {
+    "revision": "dae86f5fc8b3310e280e2f23dfa220ee",
+    "url": "/Class-Schedule-React/static/js/2.7f73729d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf957aa6cbcaa876dc2",
+    "url": "/Class-Schedule-React/static/js/main.facd0a15.chunk.js"
+  },
+  {
+    "revision": "83537716b253fdbef202",
+    "url": "/Class-Schedule-React/static/js/runtime-main.07334431.js"
+  }
+]);
