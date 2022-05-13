f750818e0d3829565d9565bedd0034db7b019928
Owilly <pasaclouma54@gmail.com>
2022-05-13 08:03:52 +0300

    deployment

---
+++ build/precache-manifest.c6db97e405191bbd01cd874e7cbfaa11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2429a93375a341ae82e3290a82871532",
+    "url": "/Class-Schedule-React/index.html"
+  },
+  {
+    "revision": "8b512fdaae3a9eaccae4",
+    "url": "/Class-Schedule-React/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "1ff41f34aab7653c16bf",
+    "url": "/Class-Schedule-React/static/css/main.8e898482.chunk.css"
+  },
+  {
+    "revision": "8b512fdaae3a9eaccae4",
+    "url": "/Class-Schedule-React/static/js/2.e2f3e478.chunk.js"
+  },
+  {
+    "revision": "e223d4a911b7738333b7f79c5bdb04ab",
+    "url": "/Class-Schedule-React/static/js/2.e2f3e478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff41f34aab7653c16bf",
+    "url": "/Class-Schedule-React/static/js/main.d1ab3911.chunk.js"
+  },
+  {
+    "revision": "83537716b253fdbef202",
+    "url": "/Class-Schedule-React/static/js/runtime-main.07334431.js"
+  }
+]);
